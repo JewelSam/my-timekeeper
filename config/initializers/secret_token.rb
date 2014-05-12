53fc42eceabd09478124184b7af0809987a61b8f
Chucha <mrswlasowa@gmail.com>
2014-05-12 15:00:04 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toggl::Application.config.secret_key_base = 'f006c5315b18df331064dbc167f39756ec50850d0119b12f6fb572c8c52b7fde3d6667b9d15b6fe262923b8fcb685097d9131e4dc53a5636638cee813032bc87'
